472831f03edf220e75b5aa160d1cc952f94a5249
Jahan123 <zeba.ali18797@gmail.com>
2020-06-19 10:34:37 +0530

    Factory and seeder Added

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->word,
+        "price" => $faker->numberBetween(2000,5000),
+        "stock" => $faker->randomDigit,
+        "details" => $faker->paragraph(1),
+        "discount" =>  $faker->numberBetween(10,50)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -7,6 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        "customer" => $faker->name,
+        "review" => $faker->paragraph(1),
+        "stars" => $faker->numberBetween(1,5),
+        'product_id' => App\Model\Product::all()->random()->id
     ];
 });
